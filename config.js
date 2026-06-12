89b1c3a067380795a0c117d3790684cf5d67a034
LeThienThanh <thanh.le.cit23@eiu.edu.vn>
2026-06-13 00:04:07 +0700

    Configure production API domain

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ window.SMARTLOCKER_API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? `http://${window.location.hostname}:5000/api`
-    : "https://environments-agenda-reasonable-webcams.trycloudflare.com/api";
+    : "https://api.shipmates.cloud/api";
