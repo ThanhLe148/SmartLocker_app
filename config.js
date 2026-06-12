8949e1757676967bbda05e58190bd174fe383dde
LeThienThanh <thanh.le.cit23@eiu.edu.vn>
2026-06-12 18:24:09 +0700

    Update frontend to current Cloudflare tunnel

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ window.SMARTLOCKER_API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? `http://${window.location.hostname}:5000/api`
-    : "https://alot-andrea-skins-cradle.trycloudflare.com/api";
+    : "https://environments-agenda-reasonable-webcams.trycloudflare.com/api";
